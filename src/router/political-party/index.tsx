9ad096b676d70732f9f9a213d5c766af83e3280d
Nazia <nazia.jahan@g.bracu.ac.bd>
2025-05-14 23:48:14 +0600

    removed login

--- src/router/political-party/index.tsx
+++ src/router/political-party/index.tsx
@@ -1,9 +1,9 @@
 import { PATH } from '@constants/paths';
-import NoMatch from '@containers/no-match';
-import { RouteType } from '../types';
 import PoliticalParty from '@containers/center-officer-management/controller-list/political-party';
 import CreatePoliticalParty from '@containers/center-officer-management/controller-list/political-party/CreatePoliticalParty';
 import EditPoliticalParty from '@containers/center-officer-management/controller-list/political-party/EditPoliticalParty';
+import NoMatch from '@containers/no-match';
+import { RouteType } from '../types';
 
 const politicalPartyRoutes = (): RouteType[] => [
   {
@@ -11,27 +11,18 @@ const politicalPartyRoutes = (): RouteType[] => [
     index: true,
     element: <PoliticalParty />,
     redirection: <NoMatch />,
-    permissions: {
-      auth: ['token'],
-    },
   },
   {
     id: '1.5.1.2',
     path: PATH.CREATE_POLITICAL_PARTY,
     element: <CreatePoliticalParty />,
     redirection: <NoMatch />,
-    permissions: {
-      auth: ['token'],
-    },
   },
   {
     id: '1.5.1.3',
     path: PATH.EDIT_POLITICAL_PARTY,
     element: <EditPoliticalParty />,
     redirection: <NoMatch />,
-    permissions: {
-      auth: ['token'],
-    },
   },
 ];
 
